780a56b582d623588bc769c43ddd8ad7871aa6c9
kuzey <kuzeyiyidiker@gmail.com>
2026-09-19 20:55:39 +0300

    chore: add ng-icons with heroicons set

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideIcons } from '@ng-icons/core';
+import { heroPlus, heroXMark, heroCog6Tooth, heroBell, heroUser, heroTrash, heroPencil, heroMagnifyingGlass } from '@ng-icons/heroicons/outline';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideIcons({
+      heroMagnifyingGlass,
+      heroPlus,
+      heroXMark,
+      heroCog6Tooth,
+      heroBell,
+      heroUser,
+      heroTrash,
+      heroPencil,
+    }),
+  ],
 };
